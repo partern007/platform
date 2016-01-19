b7df12313c88c730a3c4b3aefbdf0696a36025cc
libaocang <cdlibaocang@jd.com>
2016-01-19 13:49:58 +0800

    add platform sketon

---
+++ platform-dao/src/main/java/open/platform/repository/mybatis/MybatisRepository.java
@@ -0,0 +1,18 @@
+package open.platform.repository.mybatis;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+public @interface MybatisRepository {
+	String value() default "";
+
+}
---
+++ platform-web/src/main/webapp/WEB-INF/views/error/404.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%response.setStatus(200);%>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>页面不存在</title>
+</head>
+
+<body>
+	<h2>sorry, page not found</h2>
+</body>
+</html>
